e661f428f0f4b05c929ee0be43a80c88ff95a605
coolxer <lukasik4789@gmail.com>
2019-08-16 21:14:10 +0200

    prepared basic project structure

---
+++ test/embedded/test_uart/test_uart.c
@@ -0,0 +1,27 @@
+#ifdef UNIT_TEST
+
+/* test uart begin (init) function */
+void test_uart_begin()
+{
+
+}
+
+/* test uart send one char function */
+void test_uart_putchar(char c)
+{
+    
+}
+
+/* test uart flush function */
+void test_uart_flush()
+{
+
+}
+
+/* test uart end (close port) function */
+void test_uart_end() 
+{
+
+}
+
+#endif
