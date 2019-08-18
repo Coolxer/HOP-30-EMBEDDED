8220041c021220fd148fd6748be06d9422dd357e
coolxer <lukasik4789@gmail.com>
2019-08-18 14:08:45 +0200

    changed comments format

--- inc/uart.h
+++ inc/uart.h
@@ -4,18 +4,18 @@
 #include "stm32f4xx_hal.h"
 #include "uart_min.h"
 
-const uint8_t data_size = 64;  /* size of data transmission */
+const uint8_t data_size = 64;  // size of data transmission 
 
-uint8_t *data; /* array of uint8_t using for keep in received data */
+uint8_t *data; // array of uint8_t using for keep in received data 
 
-DMA_HandleTypeDef hdma_uart_rx; /* rx interface */
-DMA_HandleTypeDef hdma_uart_tx; /* tx interface */
+DMA_HandleTypeDef hdma_uart_rx; // rx interface 
+DMA_HandleTypeDef hdma_uart_tx; // tx interface 
 
-void uart_setup_dma(); /* prepares tx & rx interface to work in DMA mode */
-void uart_setup_nvic(); /* sets the nvic interrupts priorities */
+void uart_setup_dma(); // prepares tx & rx interface to work in DMA mode 
+void uart_setup_nvic(); // sets the nvic interrupts priorities 
 
-void uart_extend(); /* calls all setups and turns on the listening on usart2 */
+void uart_extend(); // calls all setups and turns on the listening on usart2 
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef* usart); /* calls if there is any data on rx receiving buffer (only if buffer has its own defined size -> @data_size) */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef* usart); // calls if there is any data on rx receiving buffer (only if buffer has its own defined size -> @data_size) 
 
 #endif // UART_H
