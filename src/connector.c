c88a5afb3939009dd491ad9cd51b2769d590d927
coolxer <lukasik4789@gmail.com>
2019-08-20 16:09:25 +0200

    written up more unit tests; all gone successfully

--- src/connector.c
+++ src/connector.c
@@ -1,53 +1,67 @@
 #include "connector.h"
 
 #include <stdlib.h> // needed to use malloc, calloc, realloc
-#include <string.h> // includes defintion of strtok function
+#include <string.h> // includes defintion and use of strtok function
 #include <stddef.h> // includes NULL value
 
 //#include "prepare_functions.h"
 
-uint8_t ***connector_parse(uint8_t* dialog)
+uint8_t ***connector_parse(uint8_t* dialog, uint8_t* size)
 {
-    uint8_t ***args = NULL, *param, *key, *value, size = 0;
+    uint8_t ***args = NULL, *param, *key, *value;
 
-	for (param = strtok((uint8_t*) dialog, "|"); param != NULL; param = strtok(NULL, "|"))
+	for (param = strtok((uint8_t*) dialog, DIALOG_DELIMITER); param != NULL; param = strtok(NULL, DIALOG_DELIMITER))
 	{
-		key = strtok_r(param, "=", &value);
-		args = (uint8_t ***) realloc(args, (++size) * sizeof(uint8_t *));
+		key = strtok_r(param, PARAM_DELIMITER, &value);
+		args = (uint8_t ***) realloc(args, ++(*size) * sizeof(uint8_t *));
 
-		args[size-1] = (uint8_t **) malloc(2 * sizeof(uint8_t *));
-        args[size-1][0] = (uint8_t *)malloc(connector_string_size(key) * sizeof(uint8_t));
-        args[size-1][1] = (uint8_t *)malloc(connector_string_size(value) * sizeof(uint8_t));
+		args[*size-1] = (uint8_t **) malloc(2 * sizeof(uint8_t *));
+        args[*size-1][0] = (uint8_t *)malloc(connector_string_size(key) * sizeof(uint8_t));
+        args[*size-1][1] = (uint8_t *)malloc(connector_string_size(value) * sizeof(uint8_t));
 
-		strcpy(args[size - 1][0], key);
-		strcpy(args[size - 1][1], value);
+		args[*size - 1][0] = key;
+		args[*size - 1][1] = value;
 	}
 
-	//free(param);
-	//free(key);
-	//free(value);
+	if((*size == 1) || ((args != NULL) && strcmp(args[0][0], "opt") != 0)) // if there is only one record or on the first place is not "opt" key, command is incorrect
+		return NULL;
 
 	return args;
 }
 
-void connector_manage_data(uint8_t ***args, uint8_t size)
+void connector_manage_data(uint8_t ***args, uint8_t* size)
 {
-	//uint8_t *opt = args[0][1], size = connector_data_size(args);
+	uint8_t *opt;
 
-	//memmove(args, args + 1, (--size) * sizeof(uint8_t *));
+	if(args == NULL) // if args are NULL send callback ERROR
+	{
+		// send error callback to RPI 3 through UART
+	}
+	else
+	{
+		opt = args[0][1], // get first value, which means operation type
+	
+		memmove(args, args + 1, --(*size) * sizeof(uint8_t *)); // move the array one place forward (removes first row with opt type)
 
-	//if(strcmp((void *)opt, "sth") == 0)
-	//	prepare_turn(args, size);
+		if(strcmp((void *)opt, "sth") == 0){}
+		//	prepare_turn(args, size);
+	}
 }
 
 uint8_t connector_string_size(uint8_t *string)
 {
-	uint8_t i = 0;
+	uint8_t i = 0; // number of letters
 
 	while(string[i] != '\0')
 		i++;
 
 	return i;
+}
+
+void connector_start(uint8_t *dialog)
+{
+	uint8_t size = 0; // number of records
+	connector_manage_data(connector_parse(dialog, &size), &size);
 }
 
 
--- test/native/test_connector/test_connector.c
+++ test/native/test_connector/test_connector.c
@@ -1,10 +1,11 @@
 //#ifdef UNIT_TEST
 
 #include <unity.h> // includes unit testing library
+#include <string.h>
 #include "connector.h"
 
 void setUp(); // default setup function
-void tearDown(); // de
+void tearDown(); // default release function
 
 /* CONNECTOR_STRING_SIZE TESTS ******************************************************/
 
@@ -28,21 +29,40 @@ void test_connector_string_size_should_be_five() // test if the string length is
 
 /* CONNECTOR_PARSE TESTS ***********************************************************/
 
-void test_connector_parse_should_give_null() // test if connector_parse returns NULL if the dialog is empty
+void test_connector_parse_should_give_null_if_dialog_empty() // test if connector_parse returns NULL if the dialog is empty
 {
-    uint8_t ***args = NULL, *dialog = "";
-    args = connector_parse(dialog);
+    uint8_t ***args = NULL, dialog[] = "", size = 0;
+    args = connector_parse(dialog, &size);
     TEST_ASSERT_NULL(args);
+}
+
+void test_connector_parse_should_give_null_if_pipes_only() // test if connector_parse returns NULL if the dialog consists pipes only
+{
+    uint8_t ***args = NULL, dialog[] = "|||||", size = 0;
+    args = connector_parse(dialog, &size);
+    TEST_ASSERT_NULL(args);
+}
+
+void test_connector_parse_should_give_null_if_one_record_only() // test if connector_parse returns NULL if only 1 record (that's incorrect, because you cannnot build correct, working command with only one sentence)
+{
+    uint8_t ***args = NULL, dialog[] = "opt=value1|", size = 0;
+    args = connector_parse(dialog, &size);
+    TEST_ASSERT_NULL(args);
+}
+
+void test_connector_parse_should_give_null_if_no_opt_parameter() // test if connector_parse return NULL if there is no opt parameter at start
+{
+   uint8_t ***args = NULL, dialog[] = "key1=value1|", size = 0;
+    args = connector_parse(dialog, &size);
+    TEST_ASSERT_NULL(args); 
 }
 
 void test_connector_parse_should_give_three_records() // test if connector_parse returns 3 records with correct keys & values
 {
-    uint8_t ***args = NULL, i;
-    uint8_t dialog[] = "key1=value1|key2=value2|key3=value3|";
+    uint8_t ***args = NULL, dialog[] = "opt=value1|key2=value2|key3=value3|", size = 0;
+    args = connector_parse(dialog, &size);
 
-    args = connector_parse(dialog);
-
-    TEST_ASSERT_EQUAL_STRING("key1", args[0][0]);
+    TEST_ASSERT_EQUAL_STRING("opt", args[0][0]);
     TEST_ASSERT_EQUAL_STRING("value1", args[0][1]);
     TEST_ASSERT_EQUAL_STRING("key2", args[1][0]);
     TEST_ASSERT_EQUAL_STRING("value2", args[1][1]);
@@ -50,23 +70,31 @@ void test_connector_parse_should_give_three_records() // test if connector_parse
     TEST_ASSERT_EQUAL_STRING("value3", args[2][1]);
 }
 
-void test_connector_parse_should_give_five_records() // test if connector_parse returns 5 records with correct keys & values
+/* CONNECTOR_PARSE TESTS ***********************************************************/
+
+void test_connector_manage_data_fixed_size_should_be_zero() // test if connector_manage_data no manipulate size if input array is NULL
 {
-    uint8_t ***args = NULL, i;
-    uint8_t dialog[] = "key1=value1|key2=value2|key3=value3|key4=value4|key5=value5|";
+    uint8_t size = 0;
+    connector_manage_data(NULL, &size);
 
-    args = connector_parse(dialog);
+    TEST_ASSERT_EQUAL_UINT8(0, size);
+}
 
-    TEST_ASSERT_EQUAL_STRING("key1", args[0][0]);
-    TEST_ASSERT_EQUAL_STRING("value1", args[0][1]);
-    TEST_ASSERT_EQUAL_STRING("key2", args[1][0]);
-    TEST_ASSERT_EQUAL_STRING("value2", args[1][1]);
-    TEST_ASSERT_EQUAL_STRING("key3", args[2][0]);
-    TEST_ASSERT_EQUAL_STRING("value3", args[2][1]);
-    TEST_ASSERT_EQUAL_STRING("key4", args[3][0]);
-    TEST_ASSERT_EQUAL_STRING("value4", args[3][1]);
-    TEST_ASSERT_EQUAL_STRING("key5", args[4][0]);
-    TEST_ASSERT_EQUAL_STRING("value5", args[4][1]);
+void test_connector_manage_data_fixed_size_should_be_two_if_three_records() // test if connector_manage_data manipulate size from 3 to 2 if array has 3 records
+{
+    uint8_t ***args = NULL, dialog[] = "opt=value1|key2=value2|key3=value3", size = 0;
+    connector_manage_data(connector_parse(dialog, &size), &size);
+
+    TEST_ASSERT_EQUAL_UINT8(2, size);
+}
+
+void test_connector_opt_value_should_be_mov() // test if strcpm builded in function property compares strings
+{
+    uint8_t ***args = NULL, dialog[] = "opt=mov|key2=value2|key3=value3", size = 0;
+
+    args = connector_parse(dialog, &size);
+
+    TEST_ASSERT_TRUE(strcmp((void *)args[0][1], "mov") == 0);
 }
 
 int main()
@@ -77,9 +105,16 @@ int main()
     RUN_TEST(test_connector_string_size_should_be_three);
     RUN_TEST(test_connector_string_size_should_be_five);
 
-    RUN_TEST(test_connector_parse_should_give_null);
+    RUN_TEST(test_connector_parse_should_give_null_if_dialog_empty);
+    RUN_TEST(test_connector_parse_should_give_null_if_pipes_only);
+    RUN_TEST(test_connector_parse_should_give_null_if_one_record_only);
+    RUN_TEST(test_connector_parse_should_give_null_if_no_opt_parameter);
     RUN_TEST(test_connector_parse_should_give_three_records);
-    RUN_TEST(test_connector_parse_should_give_five_records);
+
+    RUN_TEST(test_connector_manage_data_fixed_size_should_be_zero);
+    RUN_TEST(test_connector_manage_data_fixed_size_should_be_two_if_three_records);
+
+    RUN_TEST(test_connector_opt_value_should_be_mov);
 
     UNITY_END();
 }
