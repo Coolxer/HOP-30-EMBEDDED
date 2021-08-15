7a9e89db128265500dd7efe5b6af8300abd6b7d9
coolxer <lukasik4789@gmail.com>
2021-08-15 17:35:52 +0200

    update

--- test/1_test_data_assistant/test_data_assistant.c
+++ test/1_test_data_assistant/test_data_assistant.c
@@ -11,18 +11,18 @@ void tearDown();
 
 void test_contains_only_digits_should_give_false()
 {
-    TEST_ASSERT_EQUAL_UINT8(0, containsOnlyDigits(" "));
-    TEST_ASSERT_EQUAL_UINT8(0, containsOnlyDigits("a"));
-    TEST_ASSERT_EQUAL_UINT8(0, containsOnlyDigits(" 1"));
-    TEST_ASSERT_EQUAL_UINT8(0, containsOnlyDigits("a1"));
+    TEST_ASSERT_EQUAL_UINT8(0, containsOnlyDigits((void *)" "));
+    TEST_ASSERT_EQUAL_UINT8(0, containsOnlyDigits((void *)"a"));
+    TEST_ASSERT_EQUAL_UINT8(0, containsOnlyDigits((void *)" 1"));
+    TEST_ASSERT_EQUAL_UINT8(0, containsOnlyDigits((void *)"a1"));
 }
 
 void test_contains_only_digits_should_give_true()
 {
-    TEST_ASSERT_EQUAL_UINT8(1, containsOnlyDigits("0"));
-    TEST_ASSERT_EQUAL_UINT8(1, containsOnlyDigits("1"));
-    TEST_ASSERT_EQUAL_UINT8(1, containsOnlyDigits("12"));
-    TEST_ASSERT_EQUAL_UINT8(1, containsOnlyDigits("123"));
+    TEST_ASSERT_EQUAL_UINT8(1, containsOnlyDigits((void *)"0"));
+    TEST_ASSERT_EQUAL_UINT8(1, containsOnlyDigits((void *)"1"));
+    TEST_ASSERT_EQUAL_UINT8(1, containsOnlyDigits((void *)"12"));
+    TEST_ASSERT_EQUAL_UINT8(1, containsOnlyDigits((void *)"123"));
 }
 
 int main()
